f90f30d58511f702f28dd6695f047a9fca960f3a
Rizki Maulana <rrizki402@gmail.com>
2024-02-15 20:23:12 +0700

    Update Firebase ke dimas

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,14 +8,23 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2Nm_jFCxNDsqdcbvZbCjQ8pETGDc-rMQ",
+//   authDomain: "m-saving-fb35c.firebaseapp.com",
+//   projectId: "m-saving-fb35c",
+//   storageBucket: "m-saving-fb35c.appspot.com",
+//   messagingSenderId: "70822165476",
+//   appId: "1:70822165476:web:b4fdd35b4fc855034e25f1",
+//   measurementId: "G-SJF45STKXV"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Nm_jFCxNDsqdcbvZbCjQ8pETGDc-rMQ",
-  authDomain: "m-saving-fb35c.firebaseapp.com",
-  projectId: "m-saving-fb35c",
-  storageBucket: "m-saving-fb35c.appspot.com",
-  messagingSenderId: "70822165476",
-  appId: "1:70822165476:web:b4fdd35b4fc855034e25f1",
-  measurementId: "G-SJF45STKXV"
+  apiKey: "AIzaSyDXwrYiwiu5YIANA1t69vczKPZR5anVpFE",
+  authDomain: "msaving-b83df.firebaseapp.com",
+  projectId: "msaving-b83df",
+  storageBucket: "msaving-b83df.appspot.com",
+  messagingSenderId: "699836882089",
+  appId: "1:699836882089:web:ebd9b72b24836631985db8",
+  measurementId: "G-HBZNJETM6Y"
 };
 
 // Initialize Firebase
